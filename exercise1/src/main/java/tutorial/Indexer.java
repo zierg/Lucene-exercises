d24508b5295ce80cb9e3cfdebb40289f69899167
zierg <zierg@yandex.ru>
2016-08-12 01:44:41 +0400

    Modify for using the latest version of Lucene.

--- exercise1/src/main/java/tutorial/Indexer.java
+++ exercise1/src/main/java/tutorial/Indexer.java
@@ -1,26 +1,77 @@
 package tutorial;
 
-import java.io.File;
-import java.io.FileFilter;
-import java.io.FileReader;
-import java.io.IOException;
-
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.ru.RussianAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
-import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.document.LongPoint;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.util.Version;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Indexer
 {
 
-    public Indexer(String indexDirectoryPath)
+    public Indexer(String indexDirectoryPath) throws IOException
     {
-
+        Directory indexDirectory = FSDirectory.open(Paths.get(indexDirectoryPath));
+        Analyzer analyzer = new RussianAnalyzer();
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        config.setOpenMode(OpenMode.CREATE);
+        writer = new IndexWriter(indexDirectory, config);
     }
 
-    private IndexWriter indexWriter;
+    public void close() throws IOException{
+        writer.close();
+    }
+
+    public int createIndex(String dataDirPath) throws IOException
+    {
+        Path path = Paths.get(dataDirPath);
+        DirectoryStream<Path> stream = Files.newDirectoryStream(
+                path, (file) -> file.toString().toLowerCase().endsWith(".txt"));
+
+        for (Path file : stream)
+        {
+            long lastModified = Files.getLastModifiedTime(file).toMillis();
+            indexDoc(file, lastModified);
+        }
+
+        return writer.numDocs();
+    }
+
+    private void indexDoc(Path file, long lastModified) throws IOException
+    {
+        if (!Files.isDirectory(file)
+                && !Files.isHidden(file)
+                && Files.exists(file)
+                && Files.isReadable(file))
+        try (InputStream stream = Files.newInputStream(file)) {
+            Document doc = new Document();
+
+            Field pathField = new StringField(LuceneConstants.FILE_PATH, file.toString(), Field.Store.YES);
+            doc.add(pathField);
+            doc.add(new LongPoint(LuceneConstants.MODIFIED, lastModified));
+            doc.add(new TextField(LuceneConstants.CONTENTS, new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))));
+
+            System.out.println("adding " + file);
+            writer.addDocument(doc);
+        }
+    }
+
+    private IndexWriter writer;
 }
--- exercise1/src/main/java/tutorial/LuceneConstants.java
+++ exercise1/src/main/java/tutorial/LuceneConstants.java
@@ -2,8 +2,9 @@ package tutorial;
 
 public class LuceneConstants
 {
+    public static final String MODIFIED = "modified";
     public static final String CONTENTS = "contents";
     public static final String FILE_NAME = "filename";
-    public static final String FILE_PATH = "filepath";
+    public static final String FILE_PATH = "path";
     public static final int MAX_SEARCH = 10;
 }
